d5e1eca4693afee071739435135482fd44eb15c6
Thiago Vieira <thiagovieira@mpf.mp.br>
2024-02-21 18:29:07 -0300

    Initial commit

---
+++ src/test/java/com/tcvieira/anotaichallenge/AnotaiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcvieira.anotaichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
